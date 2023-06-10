00513013354fbd732710fd0cf909b13c40fdf1bb
bhupender8448 <113782918+bhupender8448@users.noreply.github.com>
2023-06-10 20:07:42 +0530

    first commit

---
+++ client/build/precache-manifest.55feba5a502bf748e7e87583cdeea51e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b811f505281b2f6263",
+    "url": "/static/css/main.65df809b.chunk.css"
+  },
+  {
+    "revision": "f3b811f505281b2f6263",
+    "url": "/static/js/main.97786522.chunk.js"
+  },
+  {
+    "revision": "d378fe90b2328cf1390a",
+    "url": "/static/js/runtime~main.370a959e.js"
+  },
+  {
+    "revision": "0dceda5ecbd949686ef5",
+    "url": "/static/css/2.a5977a51.chunk.css"
+  },
+  {
+    "revision": "0dceda5ecbd949686ef5",
+    "url": "/static/js/2.5cb28454.chunk.js"
+  },
+  {
+    "revision": "11213451287056a71ab7",
+    "url": "/static/js/3.f4f57a3b.chunk.js"
+  },
+  {
+    "revision": "249a99468d55feebb370150f1f505626",
+    "url": "/static/media/image.249a9946.jpg"
+  },
+  {
+    "revision": "eb7ccb84b025c38fe5ec9d0abb6d08ac",
+    "url": "/index.html"
+  }
+];
